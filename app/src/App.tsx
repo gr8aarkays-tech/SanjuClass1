402e57729cc8a7cbe2f357ca1248848358486b0e
gr8aarkays-tech <gr8aarkays-tech@users.noreply.github.com>
2026-09-22 09:32:10 +0530

    fix: update base path to /SchoolBuddy/ for renamed repo

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -22,7 +22,7 @@ export default function App() {
     <ThemeProvider>
       <AuthProvider>
         <AppProvider>
-          <BrowserRouter basename="/SanjuClass1">
+          <BrowserRouter basename="/SchoolBuddy">
             <Routes>
               {/* Public routes */}
               <Route path="/login" element={<LoginPage />} />
