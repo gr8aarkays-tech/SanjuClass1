922d6e4083b5f23b3671a517b3afa980265e3859
gr8aarkays-tech <gr8aarkays-tech@users.noreply.github.com>
2026-09-25 17:06:27 +0000

    deploy: 93bd98b661c8ec62e803f092833d2802670f0eb6

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/SanjuClass1/sw.js', { scope: '/SanjuClass1/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/SchoolBuddy/sw.js', { scope: '/SchoolBuddy/' })})}
